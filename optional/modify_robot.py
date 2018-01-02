a66175acc5e58987c12164f894ef6deebf70721a
Mikey <Th3M1k3y@users.noreply.github.com>
2018-01-02 18:47:15 +0100

    Update modify_robot.py

--- optional/modify_robot.py
+++ optional/modify_robot.py
@@ -12,7 +12,7 @@ panels = '[]'
 
 panels = json.dumps(panels)
 
-json_output = '{"public":"true","anonymous_control":"false","profanity_filter":"true","global_chat":"false","show_exclusive":"false","mute_text-to-speech":"false","mic_enabled":"true","dev_mode":"false","custom_panels":"true","panels":' + panels + "}"
+json_output = '{"public":"true","anonymous_control":"false","profanity_filter":"true","non_global_chat":"false","no_exclusive_control_button":"false","mute_text-to-speech":"false","mic_enabled":"true","dev_mode":"false","custom_panels":"true","panels":' + panels + "}"
 
 url = 'https://api.letsrobot.tv/api/v1/robots/' + str(robotID)
 response = requests.post(url, data=json_output, headers={"Content-Type": "application/json", "Authorization": "Bearer " + authToken})
